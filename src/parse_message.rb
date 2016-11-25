34bfc2a4b1e1d36ee5f57a21c2371f841f95c5e7
kadotani <kadotani@linecorp.com>
2016-11-25 17:40:40 +0900

    divide Function

--- src/parse_message.rb
+++ src/parse_message.rb
@@ -7,20 +7,37 @@ def parse_message(msg, mid)
   ret = ""
 
   if msg.match(/^私の名前は(.+)です$/) then
-	name = msg.gsub(/^私の名前は(.+)です$/, '\1')
-	sql = %q{UPDATE user SET name = ? WHERE mid = ?}
-	statement = $client.prepare(sql)
-	result = statement.execute(name,mid)
-	p result
-	ret = name + "さん。始めまして。お名前を登録しました。"
-
+	ret = register_name(msg,mid)
   elsif msg.match(/^教えて.*$/) then
-	#   help 的なやつを返す
-	ret = "教えてあげる"
+	ret = explain_help()
+  elsif msg.match(/.+の予定を教えて$/) then
+	  ret = tell_schedule(msg,mid)
   else
     ret = "わかりません。使い方を見たい時は「教えて」と言ってください"
   end
 
+  return ret
+end
+
+# 名前の登録
+def register_name(msg,mid)
+  name = msg.gsub(/^私の名前は(.+)です$/, '\1')
+  sql = %q{UPDATE user SET name = ? WHERE mid = ?}
+  statement = $client.prepare(sql)
+  result = statement.execute(name,mid)
+  ret = name + "さん。こんにちは。お名前を登録しました。"
+  retun ret
+end
+
+# ヘルプ
+def explain_help()
+  ret = "予定を教えてあげるカレンちゃんです"
+  return ret
+end
+
+# 予定を教えてあげる
+def tell_schedule(msg,mid)
+  ret = "今週の予定はXXです"
   return ret
 end
 
@@ -31,3 +48,4 @@ end
 # p parse_message("私の名前はあつしです", "dummy")
 # p parse_message("教えてカレン", "dummy")
 # p parse_message("教えて", "dummy")
+# p parse_message("今日の予定を教えて", "dummy")
