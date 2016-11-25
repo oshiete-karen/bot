ce62e4fb3667d8c522a51ec91a2deff954f28ea2
kadotani <kadotani@linecorp.com>
2016-11-25 12:51:51 +0900

    Adjust indent.

--- src/bot_callback.rb
+++ src/bot_callback.rb
@@ -21,28 +21,28 @@ class BotCallback < Sinatra::Base
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-        error 400 do 'Bad Request' end
+      error 400 do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
     events.each { |event|
-        case event
-        when Line::Bot::Event::Message
+      case event
+      when Line::Bot::Event::Message
         case event.type
-            when Line::Bot::Event::MessageType::Text
-                message = {
-                    type: 'text',
-                    text: event.message['text']
-                }
-                client.reply_message(event['replyToken'], message)
-            when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-                response = client.get_message_content(event.message['id'])
-                tf = Tempfile.open("content")
-                tf.write(response.body)
-            end
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
         end
+      end
     }
-
     "ok"
   end
+
 end
